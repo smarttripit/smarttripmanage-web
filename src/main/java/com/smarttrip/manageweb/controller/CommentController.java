501c77128b59f2c91cb8cce595ec7d7501fc4adc
zgoodstar <1361200381@qq.com>
2015-11-02 16:56:09 +0800

    修改

--- src/main/java/com/smarttrip/manageweb/controller/CommentController.java
+++ src/main/java/com/smarttrip/manageweb/controller/CommentController.java
@@ -12,12 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.github.pagehelper.PageInfo;
-import com.smarttrip.domain.Comment;
-import com.smarttrip.domain.Visitor;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import com.github.pagehelper.PageInfo;
+//import com.smarttrip.domain.Comment;
+//import com.smarttrip.domain.Visitor;
 import com.smarttrip.service.ICommentService;
 import com.smarttrip.service.IVisitorService;
 
